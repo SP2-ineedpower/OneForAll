282de452f633fa6867d54f55cc9601b3d88be1d3
Nicolas-Pecher <43647277+Nicolas-Pecher@users.noreply.github.com>
2018-10-25 13:37:56 +0200

    beginning profile page

---
+++ ineedpower/src/componants/CreateProject.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+class NewProject extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header version="newProject" />
+                <h1>This is the Edit project page</h1>
+            </div>
+        );
+    }
+}
+
+export default NewProject;
--- ineedpower/src/componants/Profile.js
+++ ineedpower/src/componants/Profile.js
@@ -1,14 +1,116 @@
 import React from 'react';
 import Header from './Header';
-class profile extends React.Component {
+import { NavLink } from 'react-router-dom'
+import '../css/profile.css';
+
+const User = {
+    userId: 1,
+    name: "Nicolas",
+    email: "nicolas.pecher@student.ehb.be",
+    experience: 0,
+    bio: "oppai",
+    schoolYear: "2",
+    subject: "dig-x swe",
+    age: "19",
+    type: "admin"
+}
+
+const projects = [
+    {
+        id: 1,
+        title: "Ineedpower",
+        owner: "nicolas",
+        tags: "c++"
+    }, {
+        id: 2,
+        title: "PowerneedI",
+        owner: "piet piraat",
+        tags: "js"
+    }
+]
+
+function Button(props) {
+    return (
+        <NavLink to="/createproject"> <button className="projbutton">Make new project</button></NavLink>
+    );
+
+}
+
+
+class Userdata extends React.Component {
+    render() {
+        return (
+            <div className="grid-userdata">
+                <div className="padding">
+                    <p className="profile">
+                        <b>Name: </b>
+                        <span>{User.name}</span>
+                    </p>
+
+                    <p className="profile">
+                        <b>Age: </b>
+                        <span>{User.age}</span>
+                    </p>
+
+                    <p className="profile">
+                        <b>Email: </b>
+                        <span>{User.email}</span>
+                    </p>
+
+                    <p className="profile">
+                        <b>Schoolyear: </b>
+                        <span>{User.schoolYear}</span>
+                    </p>
+
+                    <p className="profile">
+                        <b>subject: </b>
+                        <span>{User.subject}</span>
+                    </p>
+
+                    <p className="profile">
+                        <b>Bio: </b>
+                        <span>{User.bio}</span>
+                    </p>
+                </div>
+                <div id="wrapper">
+                    <Button />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+class MyProjects extends React.Component {
+
+    getprojects(projects) {
+
+      projects.forEach(project => {
+           console.log(project.title);
+        });
+    }
+    render() {
+        return (
+            <div>
+                <p className="padding myprojects"><b>Projecten</b></p>
+            </div>
+        );
+    }
+}
+
+
+class Profile extends React.Component {
     render() {
         return (
             <div>
                 <Header version="user" />
+                <Userdata></Userdata>
+                <MyProjects></MyProjects>
                 <h1>This is the active user page</h1>
             </div>
         );
     }
 }
 
-export default profile;
+export default Profile;
