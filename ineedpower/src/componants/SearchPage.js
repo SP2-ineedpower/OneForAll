54ce3c88950d0570cc7b842e724c345d1b189b74
Nicolas-Pecher <43647277+Nicolas-Pecher@users.noreply.github.com>
2018-11-15 11:43:59 +0100

    searchbalk

--- ineedpower/src/componants/SearchPage.js
+++ ineedpower/src/componants/SearchPage.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Header from './Header';
 import ProjectDisplay from './projectsDisplay';
 
+
+class Projects extends React.Component {
+    render() {
+        const querry = `http://localhost:5000/displayProjects/tag/${this.props.search}`;
+        console.log(querry);
+        return (
+            <ProjectDisplay title={this.props.search} fetch={querry} />
+        );
+    }
+}
+
+
 class SearchPage extends React.Component {
     render() {
-        const hash = this.props.location.hash;
+        const hash = this.props.location.hash.substr(1);
         return (
             <div>
                 <Header version="home" />
                 <div className="projectContainer">
-                    <p>test</p>
-                    <p>{hash}</p>
+                <Projects search={hash} ></Projects>
                 </div>
-
             </div>
         );
     }
