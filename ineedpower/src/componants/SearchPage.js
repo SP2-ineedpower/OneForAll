718b267bd4615930a90efbd7ef818f7febf894d4
JodyLambermont <44052599+JodyLambermont@users.noreply.github.com>
2018-11-18 17:06:43 +0100

    Searchpage (description)
    if statement / button needed for filtered search

--- ineedpower/src/componants/SearchPage.js
+++ ineedpower/src/componants/SearchPage.js
@@ -1,31 +1,63 @@
-import React from 'react';
-import Header from './Header';
-import ProjectDisplay from './projectsDisplay';
-
+import React from "react";
+import Header from "./Header";
+import ProjectDisplay from "./projectsDisplay";
+import Problems from "./CreateProject";
 
 class Projects extends React.Component {
-    render() {
-        const querry = `http://localhost:5000/displayProjects/tag/${this.props.search}`;
-        console.log(querry);
-        return (
-            <ProjectDisplay title={this.props.search} fetch={querry} />
-        );
-    }
+  render() {
+    const querry = `http://localhost:5000/displayProjects/tag/${
+      this.props.search
+    }`;
+    console.log(querry);
+    return <ProjectDisplay title={this.props.search} fetch={querry} />;
+  }
 }
 
+class UserSearch extends React.Component {
+  render() {
+    const querry = `http://localhost:5000/users/${this.props.search}`;
+    console.log(querry);
+    return <Userdata title={this.props.search} fetch={querry} />;
+  }
+}
+
+class ProblemSearch extends React.Component {
+  render() {
+      this.props.search
+    }`;
+    console.log(querry);
+    return <Problems title={this.props.search} fetch={querry} />;
+  }
+}
 
 class SearchPage extends React.Component {
-    render() {
-        const hash = this.props.location.hash.substr(1);
-        return (
-            <div>
+  render() {
+    const hash = this.props.location.hash.substr(1);
+    return (
+      //if(){    (on submit / knopje voor filter?)
+      <div>
+        <Header version="home" />
+        <div className="projectContainer">
+          <Projects search={hash} />
+        </div>
+      </div>
+
+      /*             <div>
                 <Header version="home" />
-                <div className="projectContainer">
-                <Projects search={hash} ></Projects>
+                <div className="profileContainer">
+                <UserSearch search={hash} ></UserSearch>
                 </div>
             </div>
-        );
-    }
+
+            <div>
+                <Header version="home" />
+                <div className="problemBox">
+                <ProblemSearch search={hash} ></ProblemSearch>
+                </div>
+            </div> */
+      // }     (haakje van if)
+    );
+  }
 }
 
 export default SearchPage;
