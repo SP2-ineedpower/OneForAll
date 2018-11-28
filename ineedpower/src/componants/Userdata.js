eb846b87709d20fa24125a8baab2528d6896b364
Nicolas-Pecher <43647277+Nicolas-Pecher@users.noreply.github.com>
2018-11-28 14:11:28 +0100

    search with ui

--- ineedpower/src/componants/Userdata.js
+++ ineedpower/src/componants/Userdata.js
@@ -29,6 +29,7 @@ class StudyField extends React.Component {
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.submit = this.submit.bind(this);
     }
 
     handleChange(event) {
@@ -37,6 +38,10 @@ class StudyField extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
+        this.submit();
+    }
+
+    submit() {
         const subject = this.state.value;
         fetch(`http://localhost:5000/user/studies`, {
             method: 'POST',
@@ -48,7 +53,6 @@ class StudyField extends React.Component {
                 "Content-Type": "application/json",
             }
         });
-
     }
 
     componentWillReceiveProps(nextProps) {
@@ -57,7 +61,7 @@ class StudyField extends React.Component {
 
     render() {
         return (
-            <form className="profileInput" onSubmit={this.handleSubmit}>
+            <form className="profileInput" onSubmit={this.handleSubmit} onBlur={this.submit}>
                 <label>
                     <input value={this.state.value} onChange={this.handleChange} type={this.props.type} className="textinput"></input>
                     <img src={pencil} alt="edit button" className="pencil" />
@@ -107,7 +111,7 @@ class Bio extends React.Component {
             <form onSubmit={this.handleSubmit} className="bio">
                 <label>
                     <textarea value={this.state.value} onChange={this.handleChange} className="profiletextarea" name="bio"></textarea>
-                    <button type="submit">submit</button>
+                    <button type="submit">save</button>
                 </label>
             </form>
         );
@@ -180,7 +184,6 @@ class Userdata extends React.Component {
                         </div>
                     </div>
                     <div id="wrapper">
-                        <Button active={false} />
                     </div>
                 </div>
             );
