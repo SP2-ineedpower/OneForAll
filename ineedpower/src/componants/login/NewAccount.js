03a6d98f97fb390ffbbd2442a17b4a83c7e84273
xtripiansx <xtripiansx@gmail.com>
2018-12-17 20:46:49 +0100

    GDPR norm (niet klaar)

--- ineedpower/src/componants/login/NewAccount.js
+++ ineedpower/src/componants/login/NewAccount.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../css/login.css';
-
+import logo from '../../pictures/ineedpowerlogo_v002.gif';
 
 class AccountInput extends React.Component {
     constructor(props) {
@@ -24,7 +24,7 @@ class AccountInput extends React.Component {
     }
 
     render() {
-        return <input type={this.props.type} placeholder={this.props.placeholder} required onChange={this.update} onBlur={this.handleSubmit}></input>
+        return <input type={this.props.type} className={this.props.style} placeholder={this.props.placeholder} required onChange={this.update} onBlur={this.handleSubmit}></input>
     }
 }
 
@@ -131,17 +131,21 @@ class NewAccount extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="loginMain">
+            <div className="loginContainer2">
+                <img src={logo} className="loginLogo" alt=""></img>
                 <form onSubmit={this.handleSubmit}>
-                    <button className="loginButton" onClick={this.onClick}><i class="fas fa-arrow-circle-left "></i></button>
-                    <AccountInput type={"text"} placeholder={"Firstname"} update={this.update} />
-                    <AccountInput type={"text"} placeholder={"Lastname"} update={this.update} />
-                    <AccountInput type={"email"} placeholder={"Email"} update={this.update} />
-                    <AccountInput type={"email"} placeholder={"CheckEmail"} update={this.update} />
-                    <AccountInput type={"password"} placeholder={"Password"} update={this.update} />
-                    <AccountInput type={"password"} placeholder={"CheckPassword"} update={this.update} />
+                    <button className="loginButton" onClick={this.onClick}><i className="fas fa-arrow-circle-left "></i></button>
+                    <AccountInput type={"text"} placeholder={"Firstname"} style={"inputMakeAcc"} update={this.update} />
+                    <AccountInput type={"text"} placeholder={"Lastname"} style={"inputMakeAcc"} update={this.update} />
+                    <AccountInput type={"email"} placeholder={"Email"} style={"inputMakeAcc"} update={this.update} />
+                    <AccountInput type={"email"} placeholder={"CheckEmail"} style={"inputMakeAcc"} update={this.update} />
+                    <AccountInput type={"password"} placeholder={"Password"} style={"inputMakeAcc"} update={this.update} />
+                    <AccountInput type={"password"} placeholder={"CheckPassword"} style={"inputMakeAcc"} update={this.update} />
+                    <span className="checkGDPR"><input type="checkbox" required/>You accept that we save your date</span>
                     <button className="newAccountButton">Create account</button>
                 </form>
+                </div>
             </div>
         );
     }
---
+++ ineedpower/src/componants/login/NewAccountPopup.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+//import { Button, Segment, TransitionablePortal } from 'semantic-ui-react'
+
+
+//code from https://react.semantic-ui.com/addons/transitionable-portal/#types-portal
+
+export default class NewAccountPopup extends React.Component {
+
+  render() {
+    return (
+      <div className="newProject">
+        <div className="container">
+          <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 200 200">
+            <g fill="none" fill-rule="evenodd">
+              <circle className="checkmark--circle" cx="100" cy="100" r="84.615" fill="#1F8429" />
+              <polyline class="checkmark--check" stroke="#FFF" points="76.923 130.769 123.077 130.769 123.077 38.462" transform="rotate(42 100 84.615)" />
+            </g>
+          </svg>
+          <h2>Profile saved</h2>
+          <p className="center">You can find your profile in the profile segment
+          and add/delete information in on that page.</p>
+          <NavLink to="/Login"><button className="pSave popButton">back to login</button></NavLink>
+        </div>
+      </div>
+    );
+  }
+}
